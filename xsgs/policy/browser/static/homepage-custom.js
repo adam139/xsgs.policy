cd7a2d87517f9521cbdd0aa46f49a2f818bca15d
adam139 <yuejun.tang@gmail.com>
2016-08-18 06:16:43 +0800

    fix homepage

--- xsgs/policy/browser/static/homepage-custom.js
+++ xsgs/policy/browser/static/homepage-custom.js
@@ -1,9 +1,10 @@
 require([
-  'jquery','jqswfobj','ajax-fetchimg','bootstrap-carousel'
-], function($,jqswf,ajaximg,carousel) {
+  'jquery','jqswfobj','bootstrap-carousel'
+], function($,jqswf,carousel) {
   'use strict';
 $(document).ready(function(){
-		rolltext(".roll-wrapper");
-		genswf("http://images.315ok.org/xsgs998/main.swf","transparent",223,1160,"#mainflash");
+//		rolltext(".roll-wrapper");
+	var w1=$('#mainflash').width();
+		genswf("http://images.315ok.org/xsgs998/main.swf","transparent",223,w1,"#mainflash");
 	});
 });
